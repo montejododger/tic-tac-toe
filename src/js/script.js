bf1a76684e1898b76d476fa157bf0c9594190d6a
Matthew Montejo <montejododger@yahoo.com>
2024-05-09 09:10:54 -0700

    end game added

--- src/js/script.js
+++ src/js/script.js
@@ -13,20 +13,20 @@ const WINNING_COMBINATIONS = [
 ];
 
 const cellEles = document.querySelectorAll('[data-cell]');
+const images = document.querySelectorAll(`img`);
 
 const startingEle = document.getElementById('starter');
 const winningMessageEle = document.getElementById('winningMsg');
 const restartButton = document.getElementById('restartBtn');
+
 // const winningMessageTextElement = document.querySelector(
 //     '[data-winning-message-text]'
 // );
 
 let circleTurn;
-let currentTurn;
-let playerX = [];
-let playerO = [];
 
 const GAMESTATE = {
+    0: null,
     1: null,
     2: null,
     3: null,
@@ -35,7 +35,6 @@ const GAMESTATE = {
     6: null,
     7: null,
     8: null,
-    9: null,
 };
 
 startGame();
@@ -43,40 +42,58 @@ startGame();
 restartButton.addEventListener('click', startGame);
 
 function startGame() {
+    currentTurn = X_CLASS;
+    resetGameState();
+    resetGameBoard(cellEles);
+
     startingEle.classList.toggle('flex');
     startingEle.classList.toggle('hidden');
-
-    playerX = [];
-    playerO = [];
-
-    currentTurn = X_CLASS;
-
-    resetBoard(cellEles);
 
     // setBoardHoverClass();
     // winningMessageEle.classList.remove('show');
 }
 
-function resetBoard(cells) {
+function resetGameBoard(cells) {
     cells.forEach(cell => {
-        cell.classList.remove(X_CLASS);
-        cell.classList.remove(CIRCLE_CLASS);
+        cell.classList.remove(X_CLASS, CIRCLE_CLASS);
+        cell.querySelectorAll('img').forEach(img =>
+            img.classList.add('hidden')
+        );
         cell.removeEventListener('click', handleClick);
         cell.addEventListener('click', handleClick, { once: true });
     });
 }
 
+function resetGameState() {
+    for (let i = 0; i < 9; i++) {
+        GAMESTATE[i] = null;
+    }
+}
+
 function handleClick(e) {
-    e.preventDefault();
+    // e.preventDefault();
 
     const cell = e.currentTarget;
 
-    markBoard(cell, currentTurn);
+    markBoard(cell);
 
-    boardWinner();
-    isDraw();
-    endGame();
-    switchTurns();
+    if (boardWinner()) {
+        return endGame();
+    } else if (isDraw()) {
+        return endGame();
+    } else {
+        switchTurns();
+    }
+
+    // if (boardWinner()) {
+    //     endGame();
+    // } else if (isDraw()) {
+    //     endGame();
+    // } else {
+    //     switchTurns();
+    // }
+
+    // endGame();
 }
 
 function markBoard(cell) {
@@ -101,7 +118,7 @@ function boardWinner() {
         });
     });
 
-    console.log(winner);
+    // console.log(winner);
     return winner;
 }
 
@@ -112,7 +129,10 @@ function switchTurns() {
 }
 
 function endGame() {
-    return console.log(`${currentTurn} is the winner`);
+    setTimeout(() => {
+        startingEle.classList.toggle('flex');
+        startingEle.classList.toggle('hidden');
+    }, 1500);
 }
 
 // function endGame(draw) {
@@ -126,14 +146,7 @@ function endGame() {
 //     winningMessageElement.classList.add('show');
 // }
 
-// function isDraw() {
-//     return [...cellElements].every(cell => {
-//         return (
-//             cell.classList.contains(X_CLASS) ||
-//             cell.classList.contains(CIRCLE_CLASS)
-//         );
-//     });
-// }
+function isDraw() {}
 
 // function setBoardHoverClass() {
 //     board.classList.remove(X_CLASS);
